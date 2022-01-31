1d3e55da7a7bfc0ff0dc4318d408d4b11bd53d29
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-01-31 11:30:50 +0100

    debug

--- ryanair.py
+++ ryanair.py
@@ -207,7 +207,7 @@ if __name__ == "__main__":
     [f.update(session=s, update=update) for r in closed_routes for f in r]
     
     for route in sorted(
-        iterable = filter(
+        filter(
             lambda r: not any(f.amount is None for f in r),
             closed_routes,
         ),
