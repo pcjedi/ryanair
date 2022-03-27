3c1e24c8dfaecc26a7b3479d6ee2c673141d4fa4
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-03-27 13:18:28 +0200

    fares

--- ryanair.py
+++ ryanair.py
@@ -9,6 +9,7 @@ import datetime
 import time
 import uuid
 from tenacity import retry, retry_if_exception_type, wait_exponential, stop_after_attempt
+from typing import List, Set
 
 
 class Flight:
@@ -56,7 +57,6 @@ def get_airports(session=requests):
     url = "https://www.ryanair.com/api/locate/v1/autocomplete/airports"
     airports = {airport["code"]:airport for airport in json.load(open("airports.json", "r"))}
     airports |= {airport["code"]:airport for airport in session.get(url).json()}
-    json.dump(airports, open("airports.json", "w"))
     return airports
 
 
@@ -107,13 +107,37 @@ def get_flights(origin, destination, availabilitie, session=requests, update=Non
 
 
 @cache
+def get_fare(origin, start, end, session=requests, sleep=None) -> Set[Flight]:
+    if sleep is not None:
+        time.sleep(sleep)
+    url = "https://services-api.ryanair.com/farfnd/3/oneWayFares"
+    params = {
+        "departureAirportIataCode": origin,
+        "outboundDepartureDateFrom": start.strftime("%Y-%m-%d"),
+        "outboundDepartureDateTo": end.strftime("%Y-%m-%d"),
+    }
+    fares = set()
+    for fare in session.get(url=url, params=params).json()['fares']:
+        fares.add(
+            Flight(
+                origin=fare["outbound"]["departureAirport"]["iataCode"],
+                destination=fare["outbound"]["arrivalAirport"]["iataCode"],
+                start=parser.parse(fare["outbound"]["departureDate"]),
+                end=parser.parse(fare["outbound"]["arrivalDate"]),
+                amount=fare["outbound"]["price"]["value"],
+                currency=fare["outbound"]["price"]["currencyCode"],
+            )
+        )
+    return fares
+
+@cache
 @retry(stop=stop_after_attempt(7), retry=(retry_if_exception_type(json.decoder.JSONDecodeError) | retry_if_exception_type(KeyError) | retry_if_exception_type(requests.exceptions.ConnectionError) ), wait=wait_exponential(multiplier=1, min=0, max=70))
 def get_rates(base="EUR", session=requests):
     g = session.get(f"https://api.exchangerate.host/latest?base={base}")
     return g.json()["rates"]
 
 
-def min_route(r):
+def min_route(r) -> List[Flight]:
     if r is None:
         return
     if len(r)==0:
@@ -133,17 +157,121 @@ def min_route(r):
     )[0]
 
 
-def get(r, l):
+def getter(r, l):
     if len(l)==1:
         return r[l[0]]
-    return get(r[l[0]], l[1:])
+    return getter(r[l[0]], l[1:])
 
 
-def set_none(r, l):
+def setter(r, l, value=None):
     if len(l)==1:
-        r[l[0]] = None
+        r[l[0]] = value
     else:
-        set_none(r[l[0]], l[1:])
+        setter(r[l[0]], l[1:], value)
+
+
+def routes_finder_alt(
+    root_origin_code,
+    start_within_days,
+    max_away_days,
+    min_stay_hours,
+    max_stay_hours,
+    unique_country=False,
+    max_routes=None,
+    sleep=0,
+    no_tqdm=False,
+    session=requests
+):
+    r = dict()
+    for dest in get_destinations(root_origin_code, session=session):
+        if (len(whitelist)==0 or dest in whitelist) and \
+        (len(country_whitelist)==0 or a[dest]["country"]["code"] in country_whitelist) and \
+        a[dest]["country"]["code"] not in country_blacklist and \
+        dest not in blacklist:
+            for date in tqdm(get_availabilities(root_origin_code, dest, session=session), desc=dest, disable=no_tqdm):
+                if date < datetime.date.today() + datetime.timedelta(start_within_days):
+                    for flight in get_flights(root_origin_code, dest, date, session=session, sleep=sleep):
+                        if flight not in r:
+                            r[flight] = {}
+
+    mr = min_route(r)
+    closed_routes = []
+    while mr is not None and \
+        (datetime.datetime.now() - start_time).total_seconds() < 3600 * 5 and \
+        (max_routes is not None or len(closed_routes) < max_routes):
+        for dest in tqdm(get_destinations(mr[-1].destination, session=session), desc=mr[-1].destination, disable=no_tqdm):
+            if dest==root_origin_code or \
+            dest not in {f.destination for f in mr} and \
+            (len(whitelist)==0 or dest in whitelist) and \
+            (len(country_whitelist)==0 or a[dest]["country"]["code"] in country_whitelist) and \
+            dest not in blacklist and \
+            a[dest]["country"]["code"] not in country_blacklist and \
+            (not unique_country or a[dest]["country"]["code"] not in {a[f.destination]["country"]["code"] for f in mr}):
+                for date in get_availabilities(mr[-1].destination, dest, session=session):
+                    if 0 <= (date - mr[-1].end.date()).days <= 1 + max_stay_hours / 24 and (date - mr[0].start.date()).days < max_away_days:
+                        for flight in get_flights(mr[-1].destination, dest, date, session=session, sleep=sleep):
+                            if 3600 * min_stay_hours < (flight.start - mr[-1].end).total_seconds() < 3600 * max_stay_hours:
+                                if flight.destination==root_origin_code:
+                                    closed_routes.append(mr + [flight])
+                                else:
+                                    getter(r, mr)[flight] = {}
+
+        if len(getter(r, mr))==0:
+            setter(r, mr)
+
+        mr = min_route(r)
+    return closed_routes
+
+
+def routes_finder(
+    airports,
+    root_origin_code,
+    start_within_days,
+    max_away_days,
+    unique_country=False,
+    country_whitelist=None,
+    max_routes=None,
+    sleep=None,
+    no_tqdm=False,
+    session=requests
+):
+    start_time = datetime.datetime.now()
+    r = dict()
+    for f in get_fare(
+        origin=root_origin_code,
+        start=datetime.date.today(),
+        end=datetime.date.today() + datetime.timedelta(start_within_days),
+        session=session,
+        sleep=sleep,
+    ):
+        if len(country_whitelist)==0 or airports[f.destination]["country"]["code"] in country_whitelist:
+            r[f] = {}
+
+    closed_routes = []
+    mr = min_route(r)
+
+    while mr is not None and \
+    (max_routes is None or len(closed_routes) < max_routes) and \
+    (datetime.datetime.now() - start_time).total_seconds() < 3600 * 5:
+        for days in tqdm(range(max_away_days - (mr[-1].end - mr[0].start).days), desc=f"{len(closed_routes)} routes found", disable=no_tqdm):
+            for flight in get_fare(
+                origin = mr[-1].destination,
+                start = mr[-1].end.date() + datetime.timedelta(days + 1),
+                end = mr[-1].end.date() + datetime.timedelta(days + 1),
+                session=session,
+                sleep=sleep,
+            ):
+                if flight.destination == root_origin_code:
+                    closed_routes.append(mr + [flight])
+                elif (len(country_whitelist)==0 or airports[flight.destination]["country"]["code"] in country_whitelist) and \
+                (not unique_country or airports[flight.destination]["country"]["code"] not in {airports[flight.destination]["country"]["code"] for f in mr}) and \
+                flight.destination not in {f.destination for f in mr}:
+                    getter(r, mr)[flight] = {}
+        if len(getter(r, mr))==0:
+            setter(r, mr)
+        mr = min_route(r)
+
+    return closed_routes
 
 
 if __name__ == "__main__":
@@ -168,7 +296,6 @@ if __name__ == "__main__":
     
     start_time = datetime.datetime.now()
     
-    r = dict()
 
     s = requests.Session()
     a = get_airports(session=s)
@@ -188,47 +315,20 @@ if __name__ == "__main__":
     print(f"{ len(country_blacklist) } countries blacklisted, airports: { {aa['name'] for aa in a.values() if aa['country']['code'] in country_blacklist} }")
     print({a[dest]["name"]:a[dest]["country"]["code"] not in country_blacklist for dest in a})
 
-
-    for dest in get_destinations(args.root_origin_code, session=s):
-        if (len(whitelist)==0 or dest in whitelist) and \
-        (len(country_whitelist)==0 or a[dest]["country"]["code"] in country_whitelist) and \
-        a[dest]["country"]["code"] not in country_blacklist and \
-        dest not in blacklist:
-            for date in tqdm(get_availabilities(args.root_origin_code, dest, session=s), desc=dest, disable=args.no_tqdm):
-                if date < datetime.date.today() + datetime.timedelta(args.start_within_days):
-                    for flight in get_flights(args.root_origin_code, dest, date, session=s, sleep=args.sleep):
-                        if flight not in r:
-                            r[flight] = {}
-
-    mr = min_route(r)
-    closed_routes = []
-    while (mr is not None and (datetime.datetime.now() - start_time).total_seconds() < 3600 * 5) or \
-        len(closed_routes) >= args.max_routes:
-        for dest in tqdm(get_destinations(mr[-1].destination, session=s), desc=mr[-1].destination, disable=args.no_tqdm):
-            if dest==args.root_origin_code or \
-            dest not in {f.destination for f in mr} and \
-            (len(whitelist)==0 or dest in whitelist) and \
-            (len(country_whitelist)==0 or a[dest]["country"]["code"] in country_whitelist) and \
-            dest not in blacklist and \
-            a[dest]["country"]["code"] not in country_blacklist and \
-            (not args.unique_country or a[dest]["country"]["code"] not in {a[f.destination]["country"]["code"] for f in mr}):
-                for date in get_availabilities(mr[-1].destination, dest, session=s):
-                    if 0 <= (date - mr[-1].end.date()).days <= 1 + args.max_stay_hours / 24 and (date - mr[0].start.date()).days < args.max_away_days:
-                        for flight in get_flights(mr[-1].destination, dest, date, session=s, sleep=args.sleep):
-                            if 3600 * args.min_stay_hours < (flight.start - mr[-1].end).total_seconds() < 3600 * args.max_stay_hours:
-                                if flight.destination==args.root_origin_code:
-                                    closed_routes.append(mr + [flight])
-                                else:
-                                    get(r, mr)[flight] = {}
-
-        if len(get(r, mr))==0:
-            set_none(r, mr)
-
-        mr = min_route(r)
+    closed_routes = routes_finder(
+        airports=a,
+        root_origin_code=args.root_origin_code,
+        start_within_days=args.start_within_days,
+        max_away_days=args.max_away_days,
+        unique_country=args.unique_country,
+        country_whitelist=country_whitelist,
+        max_routes=args.max_routes,
+        sleep=args.sleep,
+        no_tqdm=True,
+        session=s,
+    )
 
     print(f"found {len(closed_routes)} closed routes")
-    update = uuid.uuid4()
-    [f.update(session=s, update=update) for r in closed_routes for f in r]
 
     for route in sorted(
         filter(
@@ -245,5 +345,5 @@ if __name__ == "__main__":
             route[-1].end,
             route,
             [(a[f1.destination]["name"], str(f1.end-f1.start), str(f2.start-f1.end)) for f1,f2 in zip(route, route[1:])],
-            [f.url for f in route],
+            [(f.amount, f.url) for f in route],
         )
